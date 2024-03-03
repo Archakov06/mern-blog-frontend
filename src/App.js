48e9e5073081374080a313d3a79c3cc300ef257d
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-03-03 11:18:00 +0300

    Added Routes and Route in App.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import Container from "@mui/material/Container";
+import {Routes, Route} from 'react-router-dom';
 
-import { Header } from "./components";
-import { Home, FullPost, Registration, AddPost, Login } from "./pages";
+import {Header} from "./components";
+import {Home, FullPost, Registration, AddPost, Login} from "./pages";
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Container maxWidth="lg">
-        <Home />
-        {/*<FullPost />*/}
-        {/*<AddPost />*/}
-        {/*<Login />*/}
-        {/*<Registration />*/}
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <Header/>
+            <Container maxWidth="lg">
+                <Routes>
+                    <Route path='/' element={<Home/>}/>
+                    <Route path='/post/:id' element={<FullPost/>}/>
+                    <Route path='/add-post' element={<AddPost/>}/>
+                    <Route path='/login' element={<Login/>}/>
+                    <Route path='/register' element={<Registration/>}/>
+                </Routes>
+            </Container>
+        </>
+    );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import {BrowserRouter} from 'react-router-dom';
 import App from "./App";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -13,7 +14,9 @@ root.render(
   <React.StrictMode>
     <CssBaseline />
     <ThemeProvider theme={theme}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
