9a48ae3d0bda2a5fd6ae416b3a7afb990ba6764b
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-03-10 10:15:38 +0300

    Caught and corrected badly error with backend`s mongoDB properties, rebuilt some jsx files

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {Routes, Route} from 'react-router-dom';
 import {Header} from "./components";
 import {Home, FullPost, Registration, AddPost, Login} from "./pages";
 
+
 function App() {
+
     return (
         <>
             <Header/>
--- src/redux/slices/posts.js
+++ src/redux/slices/posts.js
@@ -1,11 +1,19 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, createAsyncThunk} from '@reduxjs/toolkit';
+import axios from "../../axios";
+
+
+export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
+
+    const {data} = await axios.get('/posts');
+    return data;
+});
 
 const initialState = {
     posts: {
         items: [],
         status: 'loading',
     },
-    tags:  {
+    tags: {
         items: [],
         status: 'loading',
     },
@@ -16,7 +24,23 @@ const initialState = {
 const postsSlice = createSlice({
     name: 'posts',
     initialState,
-    reducer: {},
+    reducers: {},
+    extraReducers: {
+        [fetchPosts.pending]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'loading';
+        },
+        [fetchPosts.fulfilled]: (state, action) => {
+            state.posts.items = action.payload;
+            console.log('Here is ' + action.payload);
+            state.posts.status = 'loaded';
+        },
+        [fetchPosts.rejected]: (state) => {
+            state.posts.items = [];
+            state.posts.status = 'error';
+
+        }
+    },
 });
 
 export const postsReducer = postsSlice.reducer;
