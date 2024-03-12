8402e5dedf277ffd421da6350acd3d50b9ca4bd0
Mikl Klimenko <klimenko.ma.1982@gmail.com>
2024-03-12 06:04:44 +0300

    Added correct one post view in FullPost.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -38,9 +38,9 @@ export const Home = () => {
                             <Post key={index} isLoading={true}/>
                         ) : (
                             <Post
-                                id={obj._id}
+                                _id={obj._id}
                                 title={obj.title}
-                                imageUrl="https://res.cloudinary.com/practicaldev/image/fetch/s--UnAfrEG8--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/icohm5g0axh9wjmu4oc3.png"
+                                imageUrl={obj.imageUrl}
                                 user={obj.user}
                                 createdAt={obj.createdAt}
                                 viewsCount={obj.viewsCount}
